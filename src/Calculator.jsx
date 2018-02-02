f39ef02f877a25f7b037ddcb1bcd895547041ae1
Natasha Rocha <nattycdb@hotmail.com>
2018-02-02 13:17:36 -0200

    display calculator

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,14 +1,38 @@
+import './style.css'
+
 const React = require('react')
-const calculator = require('./calculatorAPI')
+// const CalculatorAPI = require('./calculatorAPI')
 
-const c = new calculator()
+// const c = new CalculatorAPI()
 
-class Calculator extends React.Component {
+const buttons = [
+  'C', '(', ')', '÷',
+  '7', '8', '9', 'x',
+  '4', '5', '6', '-',
+  '1', '2', '3', '+',
+  '0', '.', '%', '=',
+]
+
+export default class Calculator extends React.Component {
+  constructor() {
+    super()
+    this.state = { result: 0, input: '' }
+  }
+
   render() {
     return (
-      <h1>ELECTRONIC CALCULATOR</h1>
+      <div className="calculatorWrapper">
+        <div className="calculator">
+          <h1 className="title">ELECTRONIC CALCULATOR</h1>
+          <div className="resultsWrapper">
+            <input type="text" value={this.state.result} />
+            <input type="text" value={this.state.input} />
+          </div>
+          <div className="buttonsWrapper">
+            { buttons.map(b => <button> {b} </button>) }
+          </div>
+        </div>
+      </div>
     )
   }
 }
-
-module.exports = Calculator
