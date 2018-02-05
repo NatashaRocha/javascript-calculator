c7e61865f691dce40e49015fc6550414b506fd9a
Natasha Rocha <nattycdb@hotmail.com>
2018-02-05 10:52:20 -0200

    add event click listeners

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,9 +1,9 @@
 import './style.css'
 
 const React = require('react')
-// const CalculatorAPI = require('./calculatorAPI')
+const CalculatorAPI = require('./calculatorAPI')
 
-// const c = new CalculatorAPI()
+const calculator = new CalculatorAPI()
 
 const buttons = [
   'C', '(', ')', '÷',
@@ -19,6 +19,11 @@ export default class Calculator extends React.Component {
     this.state = { result: 0, input: '' }
   }
 
+  handleClick(e) {
+    const button = e.target.innerHTML
+    this.setState({ result: button })
+  }
+
   render() {
     return (
       <div className="calculatorWrapper">
@@ -26,10 +31,10 @@ export default class Calculator extends React.Component {
           <h1 className="title">ELECTRONIC CALCULATOR</h1>
           <div className="resultsWrapper">
             <input type="text" value={this.state.result} />
-            <input type="text" value={this.state.input} />
+            <input type="text" name={this.state.input} />
           </div>
           <div className="buttonsWrapper">
-            { buttons.map(b => <button> {b} </button>) }
+            { buttons.map(b => <button key={b} onClick={this.handleClick}> {b} </button>) }
           </div>
         </div>
       </div>
